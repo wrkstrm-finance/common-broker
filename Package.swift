95a30094beb10296610c3de4d4ec70d2b46c9312
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2026-01-24 22:10:03 -0800

    build: drop tasty-trade-lib dependency

--- Package.swift
+++ Package.swift
@@ -32,7 +32,6 @@ let dependencies: [Package.Dependency] = {
       ),
       .package(name: "SchwabLib", path: "../schwab-lib"),
       .package(name: "TradierLib", path: "../tradier-lib"),
-      .package(name: "TastyTradeLib", path: "../tasty-trade-lib"),
     ]
   }
 
@@ -43,7 +42,6 @@ let dependencies: [Package.Dependency] = {
     .package(name: "wrkstrm-networking", url: "https://github.com/wrkstrm/wrkstrm-networking.git", from: "3.0.0"),
     .package(name: "SchwabLib", url: "https://github.com/wrkstrm/schwab-lib.git", from: "0.1.0"),
     .package(name: "TradierLib", url: "https://github.com/wrkstrm/tradier-lib.git", from: "0.1.0"),
-    .package(name: "TastyTradeLib", url: "https://github.com/wrkstrm/tasty-trade-lib.git", from: "0.1.0"),
   ]
 }()
 
@@ -62,16 +60,15 @@ let package: Package = .init(
   targets: [
     .target(
       name: "CommonBroker",
-      dependencies: [
-        .product(name: "WrkstrmMain", package: "wrkstrm-main"),
-        .product(name: "CommonLog", package: "common-log"),
-        .product(name: "WrkstrmFoundation", package: "wrkstrm-foundation"),
-        .product(name: "WrkstrmNetworking", package: "wrkstrm-networking"),
-        .product(name: "SchwabLib", package: "SchwabLib"),
-        .product(name: "TradierLib", package: "TradierLib"),
-        .product(name: "TastyTradeLib", package: "TastyTradeLib"),
-      ]
-    ),
+    dependencies: [
+      .product(name: "WrkstrmMain", package: "wrkstrm-main"),
+      .product(name: "CommonLog", package: "common-log"),
+      .product(name: "WrkstrmFoundation", package: "wrkstrm-foundation"),
+      .product(name: "WrkstrmNetworking", package: "wrkstrm-networking"),
+      .product(name: "SchwabLib", package: "SchwabLib"),
+      .product(name: "TradierLib", package: "TradierLib"),
+    ]
+  ),
     .testTarget(
       name: "CommonBrokerTests",
       dependencies: ["CommonBroker"],
